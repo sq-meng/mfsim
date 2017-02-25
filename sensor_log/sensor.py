0190f8ab0f9c1a4dceeefd2948503da38ede88d6
bravais <mengsq04@gmail.com>
2017-02-25 20:34:29 +0100

    password protection for env logger

--- sensor_log/sensor.py
+++ sensor_log/sensor.py
@@ -1,14 +1,17 @@
 from time import ctime
 from django.shortcuts import render
-
 import os
+
 
 def temp(request):
     get = request.GET
-    if get['pass'] == os.environ['MYPASSWORD']:
+    try:
+        if get['pass'] == os.environ['MYPASSWORD']:
 
-        time = ctime()
-        tag_lib = {'time': time}
-        return render(request, 'temperature.html', tag_lib)
-    else:
+            time = ctime()
+            tag_lib = {'time': time}
+            return render(request, 'temperature.html', tag_lib)
+        else:
+            return render(request, 'temperror.html', {})
+    except KeyError:
         return render(request, 'temperror.html', {})
